157f1c93927690b43c9874b7e6b08c6eb8baccb4
jt-ito <jtbjr07@gmail.com>
2025-08-29 08:23:22 -0700

    parser: remove episodeTitle and year from filename parsing; add test script

--- scripts/test-parse.js
+++ scripts/test-parse.js
@@ -1,3 +1,33 @@
+const parse = require('../lib/filename-parser');
+const examples = [
+  // user example (nested path)
+  '86 S01+SP 1080p Dual Audio BDRip 10 bits DD+ x265-EMBER/86 S01P01+SP 1080p Dual Audio BDRip 10 bits DD+ x265-EMBER/S01E01-Undertaker [2F703024].mkv',
+  // typical SxxEyy
+  'Show.Name.S02E05.1080p.WEB-DL.x264-Group.mkv',
+  // 1x02 style
+  'Another.Show.1x02.HDTV-LOL.mkv',
+  // numeric title
+  '86. S01E02.720p.mkv',
+  // dash separated
+  'Some Show - S01E03 - The Episode Title - 1080p.mkv',
+  // special marker
+  'Series.Title.S01P01.Special.1080p.mkv',
+  // multi-episode
+  'Show - 01-02 - Double Ep - 720p.mkv',
+  // episode number at end
+  'WeirdShow 05 720p.mkv',
+  // with year in title
+  'Movie.Title.2019.1080p.BluRay.mkv',
+  // messy release name
+  'Anime.Title.S03E12.1080p.WEBRip.x265.AAC-Group [eng].mkv'
+];
+
+for (const e of examples) {
+  const parsed = parse(e);
+  console.log('---');
+  console.log('input:', e);
+  console.log(JSON.stringify(parsed, null, 2));
+}
 // quick test harness for filename parsing in server.js
 const path = require('path');
 const server = require(path.join(__dirname, '..', 'server.js'));
