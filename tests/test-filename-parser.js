7a8efe0005ba7b65dd56a08d94ba0b1a30dd0fff
jt-ito <jtbjr07@gmail.com>
2025-09-15 18:51:45 -0700

    Do not use RHS after leading SxxEyy as series title; add filename-parser test

---
+++ tests/test-filename-parser.js
@@ -0,0 +1,23 @@
+const assert = require('assert')
+const parse = require('../lib/filename-parser')
+
+function run() {
+  // Example path from the user
+  const path = '/mnt/Tor/Aparida S01 1080p Dual Audio WEBRip DD+ x265-EMBER/S01E01-Clover Is Born [76538209].mkv'
+  const res = parse(path)
+
+  // We expect the series title NOT to be the episode title 'Clover Is Born'.
+  // The parser should preserve the left-side series title (from the folder name) like 'Aparida S01' -> cleaned to 'Aparida'.
+  assert.ok(res.title && res.title.toLowerCase().includes('aparida'), `expected title to contain 'aparida', got '${res.title}'`)
+
+  // Season/Episode should be parsed
+  assert.strictEqual(res.season, 1, `expected season 1 got ${res.season}`)
+  assert.strictEqual(res.episode, 1, `expected episode 1 got ${res.episode}`)
+
+  // parsedName should not contain the episode title 'Clover Is Born'
+  assert.ok(!/Clover Is Born/i.test(res.parsedName), `parsedName should not include episode title, got '${res.parsedName}'`)
+
+  console.log('filename-parser tests passed')
+}
+
+run()
