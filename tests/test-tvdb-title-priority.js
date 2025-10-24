645b94455b04b808b1478d5c5adb2b58b73629fe
jt-ito <jtbjr07@gmail.com>
2025-10-23 23:47:44 -0700

    tvdb: prefer english episode titles (english -> romaji -> native)

---
+++ tests/test-tvdb-title-priority.js
@@ -0,0 +1,32 @@
+const assert = require('assert')
+const { extractEpisodeTitle } = require('../lib/tvdb')
+
+function run() {
+  const episodeWithEnglish = {
+    name: '故郷と旅路',
+    translations: [
+      { language: 'jpn', name: '故郷と旅路' },
+      { language: 'eng', name: 'Home and Journey' },
+      { language: 'x-jat', name: 'Furusato to Tabiji' }
+    ]
+  }
+  assert.strictEqual(extractEpisodeTitle(episodeWithEnglish), 'Home and Journey')
+
+  const episodeWithRomajiOnly = {
+    name: '故郷と旅路',
+    translations: [
+      { language: 'x-jat', name: 'Furusato to Tabiji' },
+      { language: 'jpn', name: '故郷と旅路' }
+    ]
+  }
+  assert.strictEqual(extractEpisodeTitle(episodeWithRomajiOnly), 'Furusato to Tabiji')
+
+  const episodeNativeOnly = {
+    name: '故郷と旅路'
+  }
+  assert.strictEqual(extractEpisodeTitle(episodeNativeOnly), '故郷と旅路')
+
+  console.log('extractEpisodeTitle tests passed')
+}
+
+run()
